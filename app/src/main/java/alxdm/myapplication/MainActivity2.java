2093c543e5a813a0bf3c80c2aed00390bc457ef4
woltron <woltron@mail.ru>
2015-05-28 17:54:39 +0300

    PIZZA-1 Сделал несколько бесполезных переименований и выгрузил правильное форматирование для идеи

--- app/src/main/java/alxdm/myapplication/MainActivity2.java
+++ app/src/main/java/alxdm/myapplication/MainActivity2.java
@@ -9,39 +9,44 @@ import android.view.View;
 //import android.content.Intent;
 
 
-public class MainActivity3Activity extends ActionBarActivity {
+public class MainActivity2 extends ActionBarActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_activity3);
+        setContentView(R.layout.activity_main_activity2);
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_activity3, menu);
+        getMenuInflater().inflate(R.menu.menu_main_activity2, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    public void onClick(View view) {
-        Intent intent = new Intent(MainActivity3Activity.this, MainActivity.class);
-        startActivity(intent);
+    public void onClick(View view)
+    {
+        startActivity(new Intent(MainActivity2.this, MainActivity3.class));
     }
 }
